2d651cc66683b58777dccbcbff02e74aefb062c2
raquellucenap <114938048+raquellucenap@users.noreply.github.com>
2023-02-19 15:24:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,8 @@ if st.button("RUN"):
     # Store the M3U8 playlist.
     with open(m3u_filepaths_file, 'w') as f:
         # Modify relative mp3 paths to make them accessible from the playlist folder.
-        mp3_paths = [os.path.join('..', mp3) for mp3 in mp3s]
+        #mp3_paths = [os.path.join('..', mp3) for mp3 in mp3s]
+        #mp3_paths = [mp3 for mp3 in mp3s]
         f.write('\n'.join(mp3_paths))
         st.write(f'Stored M3U playlist (local filepaths) to `{m3u_filepaths_file}`.')
 
