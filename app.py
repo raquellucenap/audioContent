540799ece26ffd6cc78e8f6619892933c7c23460
raquellucenap <114938048+raquellucenap@users.noreply.github.com>
2023-02-21 11:30:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,20 +2,20 @@ import os.path
 import random
 import streamlit as st
 import pandas
+import pandas as pd
+import pickle
 
 
 m3u_filepaths_file = 'playlists/streamlit.m3u8'
-ESSENTIA_ANALYSIS_PATH = 'data/files_essentia_effnet-discogs.jsonl.pickle'
+ESSENTIA_ANALYSIS_PATH = 'data/featuresAllAgain.csv'
+with open(ESSENTIA_ANALYSIS_PATH,'rb') as f:
+    data = pd.read_csv(f)
+audio_analysis = data
 
-
-def load_essentia_analysis():
-    return pandas.read_pickle(ESSENTIA_ANALYSIS_PATH)
-
-
+st.dataframe(audio_analysis)
 st.write('# Audio analysis playlists example')
 st.write(f'Using analysis data from `{ESSENTIA_ANALYSIS_PATH}`.')
-audio_analysis = load_essentia_analysis()
-audio_analysis_styles = audio_analysis.columns
+audio_analysis_styles = audio_analysis["style"].unique()
 st.write('Loaded audio analysis for', len(audio_analysis), 'tracks.')
 
 st.write('## 🔍 Select')
@@ -23,16 +23,38 @@ st.write('### By style')
 st.write('Style activation statistics:')
 st.write(audio_analysis.describe())
 
-style_select = st.multiselect('Select by style activations:', audio_analysis_styles)
+style_select = st.multiselect('Select by style:', audio_analysis_styles)
 if style_select:
-    # Show the distribution of activation values for the selected styles.
-    st.write(audio_analysis[style_select].describe())
-
+    st.write('Select tracks with `{style_select}` styles.')
+    
+style_select_all = st.multiselect('Select by style activations:', audio_analysis_styles)
+if style_select_all:
+    st.write('Select tracks by style activations `{style_select_all}` styles.')
     style_select_str = ', '.join(style_select)
     style_select_range = st.slider(f'Select tracks with `{style_select_str}` activations within range:', value=[0.5, 1.])
+    
+st.write('## Select Tempo⏲️')
+tempo_select_range = st.slider('Select a range', 60,185, (60,185))
+st.write("You selected the range:", tempo_select_range)
 
-st.write('## 🔝 Rank')
-style_rank = st.multiselect('Rank by style activations (multiplies activations for selected styles):', audio_analysis_styles, [])
+st.write('## Select Voice or instrument')
+vi_checkbox = st.checkbox('Select voice!')
+if vi_checkbox:
+    st.write('Voice is selected!🎤👄')
+else:
+    st.write('Instrument is selected!🎻🎺')
+    
+st.write('## Select Danceability🕺🏻💃🪩')
+danceability_select_range = st.slider('Select a range', 0,3, (0,3))
+st.write("You selected the range:", danceability_select_range)
+
+st.write('## Select Arousal')
+arousal_select_range = st.slider('Select a range', 1,9, (1,9))
+st.write("You selected the range:", arousal_select_range)
+
+st.write('## Select valence')
+valence_select_range = st.slider('Select a range', 1,8, (1,8))
+st.write("You selected the range:", valence_select_range)
 
 st.write('## 🔀 Post-process')
 max_tracks = st.number_input('Maximum number of tracks (0 for all):', value=0)
@@ -40,34 +62,26 @@ shuffle = st.checkbox('Random shuffle')
 
 if st.button("RUN"):
     st.write('## 🔊 Results')
-    mp3s = list(audio_analysis.index)
-
+    result=audio_analysis.loc[(audio_analysis['tempo'] >= tempo_select_range[0]) & (audio_analysis['tempo'] <= tempo_select_range[1])]
+    result=result.loc[(result['danceability'] >= danceability_select_range[0]) & (result['danceability'] <= danceability_select_range[1])]
+    result = result.loc[(result["arousal"] >= arousal_select_range[0]) & (result["arousal"] <= arousal_select_range[1])]
+    result = result.loc[(result["valence"] >= valence_select_range[0]) & (result["valence"] <= valence_select_range[1])]
+    if vi_checkbox:
+        result = result.loc[result["instrumentalvoice"] == "voice"]
+    else:
+        result = result.loc[result["instrumentalvoice"] == "instrumental"]
     if style_select:
-        audio_analysis_query = audio_analysis.loc[mp3s][style_select]
-
-        #for style in style_select:
-        #    fig, ax = plt.subplots()
-        #    ax.hist(audio_analysis_query[style], bins=100)
-        #    st.pyplot(fig)
-
-        result = audio_analysis_query
-        for style in style_select:
-            result = result.loc[result[style] >= style_select_range[0]]
-        st.write(result)
-        mp3s = result.index
-
-    if style_rank:
-        audio_analysis_query = audio_analysis.loc[mp3s][style_rank]
-        audio_analysis_query['RANK'] = audio_analysis_query[style_rank[0]]
-        for style in style_rank[1:]:
-            audio_analysis_query['RANK'] *= audio_analysis_query[style]
-        ranked = audio_analysis_query.sort_values(['RANK'], ascending=[False])
-        ranked = ranked[['RANK'] + style_rank]
-        mp3s = list(ranked.index)
-
-        st.write('Applied ranking by audio style predictions.')
-        st.write(ranked)
-
+        result = result.loc[result["style"].isin(style_select)]
+    if style_select_all:
+        audio_analysis_query = result.loc[result.index][style_select_all]
+        result1 = audio_analysis_query
+        for style_all in style_select_all:
+            result1 = result1.loc[result1[style_all]>=style_select_range[0]]
+        st.write(result1)
+        #result = result.loc[result["style"].isin(style_select)]
+    audio_analysis = result
+    #mp3s = list(audio_analysis.index)
+    mp3s = list(audio_analysis["audiofile"])
     if max_tracks:
         mp3s = mp3s[:max_tracks]
         st.write('Using top', len(mp3s), 'tracks from the results.')
@@ -79,7 +93,9 @@ if st.button("RUN"):
     # Store the M3U8 playlist.
     with open(m3u_filepaths_file, 'w') as f:
         # Modify relative mp3 paths to make them accessible from the playlist folder.
-        mp3_paths = [os.path.join('..', mp3) for mp3 in mp3s]
+        #mp3_paths = [os.path.join('/C:/Users/SMPC001/Downloads/MusAV/audio_chunks', mp3) for mp3 in mp3s]
+        mp3_paths = [mp3 for mp3 in mp3s]
+        st.write(mp3_paths)
         f.write('\n'.join(mp3_paths))
         st.write(f'Stored M3U playlist (local filepaths) to `{m3u_filepaths_file}`.')
 
